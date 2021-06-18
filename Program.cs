94940982dd41b5e239d5af2f487a247a2556dd57
Cab38-03 <Cab38-03@03-C38-03>
2021-06-18 12:42:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test11
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = { 0, 1, 2, 30, 4, 5, 6, 0, 7, 8, 0, 9, 10 };
+            Console.WriteLine($"Количество нулей - {array.Count(e => e == 0)}");
+        }
+    }
+    
+}
